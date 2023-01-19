3c0e06238200c80fb9684a3c74cbdc926b99a6c7
Vinny021 <viniciusbarbosa021@gmail.com>
2023-01-19 17:46:19 -0300

    Csv input box

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='csvInput'>
+        <textarea name="csvInput" id="csvInput"></textarea>
+      </div>
     </div>
   );
 }
