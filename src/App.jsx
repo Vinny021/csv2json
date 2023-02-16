42a7e86728499daadbadb7ed69425a3440ba01aa
Vinny021 <viniciusbarbosa021@gmail.com>
2023-02-16 19:29:36 -0300

    Permitir editar Json

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,17 @@ function App() {
   const [csvText, setCsvText] = useState('');
   const [jsonText, setJsonText] = useState('');
   const [showAlert, setShowAlert] = useState(false);
+  const [csvTitle, setCsvTitle] = useState('CsvSelected');
+  const [jsonTitle, setJsonTitle] = useState('JsonUnselected');
 
-  const handleChange = (event) => {
+  const handleCsvChange = (event) => {
     const text = event.target.value
     setCsvText(text);
+  }
+  
+  const handleJsonChange = (event) => {
+    const text = event.target.value
+    setJsonText(text);
   }
 
   const validateCsv = (csvSplited) => {
@@ -56,19 +63,41 @@ function App() {
     setJsonText(JSON.stringify(jsonArray));
   }
 
+  const changeFocusConverted = (event) => {
+    const className = event.target.className;
+
+    if(className === 'CsvUnselected'){
+      setCsvTitle('CsvSelected');
+      setJsonTitle('JsonUnselected');
+    }
+
+    if(className === 'JsonUnselected'){
+      setCsvTitle('CsvUnselected');
+      setJsonTitle('JsonSelected');
+    }
+  }
+
   var alertDiv = showAlert ? <AlertComponent/> : null
+
+  var csvTextArea = csvTitle === 'CsvSelected' ? 
+  <textarea name="csvInput" id="csvInput" onChange={handleCsvChange}></textarea> :
+  <textarea name="csvInput" id="csvInput" value={csvText}></textarea>;
+  
+  var jsonTextArea = jsonTitle === 'JsonSelected' ? 
+  <textarea name="jsonOutput" id="jsonOutput" onChange={handleJsonChange}></textarea> :
+  <textarea name="jsonOutput" id="jsonOutput" value={jsonText}></textarea>;
 
   return (
     <div className="App">
       {alertDiv}
       <div className='CsvJsonFields'>
         <div className='CsvInput'>
-          <h1 className='CsvTitle'>CSV</h1>
-          <textarea name="csvInput" id="csvInput" onChange={handleChange}></textarea>
+          <h1 onClick={changeFocusConverted} className={csvTitle}>CSV</h1>
+          {csvTextArea}
         </div>
         <div className='JsonOutput'>
-          <h1 className='JsonTitle'>JSON</h1>
-          <textarea name="jsonOutput" id="jsonOutput" value={jsonText}></textarea>
+          <h1 onClick={changeFocusConverted} className={jsonTitle}>JSON</h1>
+          {jsonTextArea}
         </div>
       </div>
       <button id="success-btn" className='Convert2Json' onClick={csv2json}>Converter para JSON</button>
